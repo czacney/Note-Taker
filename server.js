52e8e8ed4ae82caf70f7a341e270e092d8b9c980
Casey Zacney <czacney@gmail.com>
2020-03-08 20:48:38 -0400

    updated app.get

--- server.js
+++ server.js
@@ -47,15 +47,7 @@ app.post("/api/notes", function (req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
     var newNote = req.body;
-
-    // Using a RegEx Pattern to remove spaces from newCharacter
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newNote.routeName = newNote.name.replace(/\s+/g, "").toLowerCase();
-
-    console.log(cd);
-
     dbnote.push(newNote);
-
     res.json(newNote);
 });
 
