8598acbb51a269866de297fffae6d7aa822936e5
Casey Zacney <czacney@gmail.com>
2020-03-08 20:24:29 -0400

    updated code, tried to link to db json

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Note-Taker
+
+Deployed link: https://note-taker-69.herokuapp.com/
--- server.js
+++ server.js
@@ -1,17 +1,32 @@
-const express = requier("express");
+const express = require("express");
 const path = require("path");
 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+let dbnote = require("./db/db.json");
+
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static('public'));
+
+
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+
+app.get("/api/notes/:character", function(req, res) {
+    var chosen = req.params.character;  
+
 
 
 app.listen(PORT, function () {
