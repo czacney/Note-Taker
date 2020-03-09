6a35517d95df7211da98429c8e960ba72fb4aa9a
Casey Zacney <czacney@gmail.com>
2020-03-08 21:10:04 -0400

    updated db because dbnote was confusing

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs")
 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-let dbnote = require("./db/db.json");
+let db = require("./db/db.json");
 
-
-// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -18,25 +17,20 @@ app.use(express.static('public'));
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
-
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
 });
-
-
 app.get("/api/notes", function (req, res) {
-    return res.json(dbnote);
+    return res.json(db);
 });
 
 
 app.get("/api/notes/:notes", function (req, res) {
     var chosen = req.params.notes;
 
-    console.log(chosen);
-
-    for (var i = 0; i < dbnote.length; i++) {
-        if (chosen === dbnote[i].routeName) {
-            return res.json(dbnote[i]);
+    for (var i = 0; i < db.length; i++) {
+        if (chosen === db[i].routeName) {
+            return res.json(db[i]);
         }
     }
 
@@ -44,10 +38,8 @@ app.get("/api/notes/:notes", function (req, res) {
 });
 
 app.post("/api/notes", function (req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
     var newNote = req.body;
-    dbnote.push(newNote);
+    db.push(newNote);
     res.json(newNote);
 });
 
